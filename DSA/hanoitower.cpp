11fddb88b240cdc9a132ca63a2efa90f06ef3c37
Rahul Yadav <imdotrahul@gmail.com>
2021-10-11 20:59:09 +0530

    code changes

--- DSA/hanoitower.cpp
+++ DSA/hanoitower.cpp
@@ -1,14 +1,24 @@
-#include<iostream>
+#include <bits/stdc++.h>
 using namespace std;
-
-int solution(int x)
+ 
+void towerOfHanoi(int n, char from_rod,
+                    char to_rod, char aux_rod)
 {
-    
+    if (n == 1)
+    {
+        cout << "Move disk 1 from rod " << from_rod <<
+                            " to rod " << to_rod<<endl;
+        return;
+    }
+    towerOfHanoi(n - 1, from_rod, aux_rod, to_rod);
+    cout << "Move disk " << n << " from rod " << from_rod <<
+                                " to rod " << to_rod << endl;
+    towerOfHanoi(n - 1, aux_rod, to_rod, from_rod);
 }
+ 
 int main()
 {
-    int numberofdiscs,result;
-    cin>>numberofdiscs;
-    result=solution(numberofdiscs);
-
+    int n = 4; 
+    towerOfHanoi(n, 'A', 'C', 'B'); 
+    return 0;
 }

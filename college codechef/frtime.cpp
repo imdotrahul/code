4b6c54a7536d4615239aac345e05ead1bd56bdd5
Rahul Yadav <imdotrahul@gmail.com>
2021-10-30 23:20:47 +0530

    free time solved

--- college codechef/frtime.cpp
+++ college codechef/frtime.cpp
@@ -1,20 +1,48 @@
-#include<iostream>
-#include<algorithm>
-#include<tuple>
-#include<vector>
+#include <iostream>
+#include <vector>
+#include <tuple>
+#include <algorithm>
 using namespace std;
 
-
-int main ()
+int main()
 {
-    int a,b;
-    cin >>a>>b;
-    vector<pair<int,int>> alice;
-    vector<pair<int,int>> bob;
-    int c,d;
-    for(int i=0;i<a;i++)
+    ios::sync_with_stdio(false);
+    cin.tie();
+    long long n, k;
+    cin >> n >> k;
+    vector<pair<long long , long long> > a, b;
+    int x,  y;
+    for (int i = 0; i < n; i++) 
     {
-        cin>>c>>d;
-        
+        cin >> x >> y;
+        a.push_back(make_pair(x, y));
     }
+    for (int i = 0; i < k; i++) 
+    {
+        cin >> x >> y;
+        b.push_back(make_pair(x, y));
+    }
+    sort(a.begin(), a.end());
+    sort(b.begin(), b.end());
+    long long i = 0, j = 0;
+    long long sum = 0;
+    while(i < n && j < k)
+    {
+        if (a[i].first < b[j].second && a[i].second>b[j].first)
+        {
+            sum += abs(max(a[i].first, b[j].first) - min(a[i].second, b[j].second));
+        }
+        if(j+1 < k && a[i].second > b[j+1].first) {
+            j++;
+        }
+        else if (i+1 < n && a[i+1].first < b[j].second)
+        {
+            i++;
+        } else {
+            i++;
+            j++;
+        }
+    }
+    cout << sum << '\n';
+    return 0;
 }
--- college codechef/palindrome.cpp
+++ college codechef/palindrome.cpp
@@ -1,26 +1,15 @@
 #include<iostream>
 #include<string.h>
-=using namespace std;
- 
-// Function to check if it is possible
-// to convert the string into palindrome
+using namespace std;
 bool checkPalindrome(string str){
      
-    int n = str.length();       
-   
-    // Counting number of characters
-    // that should be changed.
+    int n = str.length();   
     int count = 0;
     for (int i = 0; i < n/2; ++i)
         if (str[i] != str[n - i - 1])
             ++count;
-     
-    // If count of changes is less than
-    // or equal to 1
     return (count <= 1);
 }
- 
-// Driver function.
 int main()
 {
     string str = "abccaa";   

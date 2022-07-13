3ea45430fe0d267ef1523db442cbb41ed05df0d6
Rahul Yadav <imdotrahul@gmail.com>
2022-07-13 22:17:30 +0530

    starter codechef july

---
+++ codechef updated/mincoinsreq.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int x;
+        cin>>x;
+        int result = x%10;
+        cout<<result<<"\n";
+    }
+}
---
+++ codechef updated/newcode.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a=3,b=1,c=2;
+    b=b^a^c*2;
+    if(b && c)
+    {
+        b=1;
+        if(a)
+        {
+            a=a*a%5;
+        }
+        c=0;
+    }
+    cout<<a+b+c;
+}

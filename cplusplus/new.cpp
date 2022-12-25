ecbbab2cdb42952dab5f25352543d882bd580ce5
Rahul Yadav <imdotrahul@gmail.com>
2022-12-25 15:47:49 +0530

    previous files

--- cplusplus/new.cpp
+++ cplusplus/new.cpp
@@ -1,26 +1,20 @@
 #include<iostream>
+#include<stack>
 using namespace std;
+
+
+void  multiply(int a,int b)
+{
+    int x = a*b;
+    cout <<x;
+    
+}
+
 
 int main()
 {
-    ios_base::sync_with_stdio(false);  
-    cin.tie(NULL);
-    int testcase;
-    cin>>testcase;
-    while(testcase--)
-    {
-        int n;
-        cin>>n;
-        int arr[n];
-        int count=0;
-        for(int i=0;i<n;i++)
-        {
-            cin>>arr[i];
-            if(arr[i]>=1000)
-            {
-                count++;
-            }
-        }
-        cout<<count<<"\n";
-    }
+     multiply(5,6);
+    
+    
+    
 }

f0e957c3e14cc3a02fa5ccd5b73eea9dd6bc5c37
imdotrahul <imdotrahul@gmail.com>
2023-06-24 16:30:42 +0530

    practice

---
+++ college codechef/chefmon.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+uisng namespace std;
+
+
+int int main() {
+    int testcases;
+    cin>> testcases;
+
+    while(testcases--)
+    {
+        int n,b;
+        cin>> n>>b;
+        int arr[n];
+        for(int i = 0;i<n;i++)
+
+    }
+
+
+    return 0;
+}
--- college codechef/maxdiff.cpp
+++ college codechef/maxdiff.cpp
@@ -1,31 +1,62 @@
-#include<iostream>
-#include<algorithm>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <utility>
 using namespace std;
 
-int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-	int t;
-	cin>>t;
-	while(t--){
-	    int N,K;
-	    cin>>N>>K;
-	    
-	    int arr[N];
-	    int m=0;
-	    for(int i=0; i<N; i++){
-	        cin>>arr[i];
-	        m+=arr[i];
-	    }
-	    
-	    sort(arr, arr+N);
-	    int A=0, B=0;
-	    for(int i=0; i<K; i++){
-	        A+=arr[i];
-	        B+=arr[(N-1)-i];
-	    }
-	    
-	    cout<<max(abs((m-A)-A), abs((m-B)-B))<<"\n";
-	}
-	return 0;
+bool isHarshad(int num) {
+    int digitSum = 0;
+    std::string numStr = std::to_string(num);
+
+    for (char digit : numStr) {
+        digitSum += digit - '0';
+    }
+
+    return (num % digitSum == 0);
+}
+
+int countSubarraysWithHarshadNumbers(const std::vector<int>& A, const std::vector<std::pair<int,int>>& queries) {
+    int N = A.size();
+    std::vector<int> prefix(N + 1, 0);
+
+    for (int i = 1; i <= N; i++) {
+        prefix[i] = prefix[i - 1] + isHarshad(A[i - 1]);
+    }
+
+    int result = 0;
+    for (const auto& query : queries) {
+        int L = query.first;
+        int R = query.second;
+        int count = prefix[R] - prefix[L - 1];
+        result += count;
+    }
+
+    return result;
+}
+
+int main()
+{
+    int testcases;
+    cin>>testcases;
+    
+    while(testcases--)
+    {
+        int number,query,x;
+        cin>> number>>query;
+        std::vector<int> arr[number];
+        
+        for(int i = 0;i<number;i++)
+        {
+            cin>>arr[i];
+        }
+        while(query--)
+        {
+            int left,right;
+            cin>>left>>right; 
+            
+            int result = countSubarraysWithHarshadNumbers(arr, query);
+            cout<<result;
+            
+        }
+    }
 }

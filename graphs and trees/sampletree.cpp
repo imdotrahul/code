0a00bf75002a4cca999446e15f5d1a0010e4bf10
RAHUL YADAV <imdotrahul@gmail.com>
2024-08-28 16:23:45 +0530

    learned operations on trees

--- graphs and trees/sampletree.cpp
+++ graphs and trees/sampletree.cpp
@@ -13,7 +13,7 @@ struct Node{
     }   
 };
 
-Node*insert(Node*root,int val)
+Node*insert(Node*root,int val)// insert node into a tree
 {
 
     if(root  == NULL)
@@ -37,16 +37,16 @@ Node*insert(Node*root,int val)
 
 }
 
-void printPreorder(Node*head){
+void printPreorder(Node*head){ //print Preorder Traversal
     if(head==NULL)
     {
         return ;
     }
-    cout<<head->data;
+    cout<<head->data<<" ";
     printPreorder(head->left);
     printPreorder(head->right);
 }
-void Inorder(Node*head)
+void Inorder(Node*head)//Print Inorder traveral
 {
     if(head==NULL)
     {
@@ -54,11 +54,11 @@ void Inorder(Node*head)
     }
 
     Inorder(head->left);
-    cout<<head->data;
+    cout<<head->data<<" ";
     Inorder(head->right);
 
 }
-void postorder(Node*head)
+void postorder(Node*head) // Print postorder traversal
 {
     if(head==NULL)
     {
@@ -66,7 +66,98 @@ void postorder(Node*head)
     }
     postorder(head->left);
     postorder(head->right);
-    cout<<head->data;
+    cout<<head->data<<" ";
+}
+string search(Node*head,int val) // search an item in a tree with a given value
+{
+    if(head==NULL){
+        return "Not Found";
+
+    }
+    if(head->data==val)
+    {
+        return "found";
+    }
+    if(head->data<val){
+         return search(head->right,val);
+    }
+    else{
+       return  search(head->left,val);
+    }
+
+}
+void findminimum(Node*head) //find minimum inside a binary tree
+{
+    Node*n;
+    n= head;
+    while(n->left!= NULL){
+        n= n->left;
+    }
+    cout<<"The minimum number is: "<<n->data;
+
+}
+void findmaximum(Node* head){//find maximum inside a binary tree
+    Node*n;
+    n= head;
+    while(n->right!= NULL){
+        n= n->right;
+    }
+    cout<<"The maximum number is: "<<n->data;
+
+
+}
+Node*findmin(Node*root)//function for deletion of node wehere right and left subtree occurs
+{
+    while(root && root->left!= NULL)
+    {
+        root= root->left;
+    }
+    return root;
+}
+
+Node* deletenode(Node*head, int val)
+{
+    if(head == NULL)
+    {
+        return head; 
+    }
+
+    if(head->data>val)
+    {
+        head->left= deletenode(head->left,val);
+    }
+    if(head->data<val)
+    {
+        head->right= deletenode(head->right,val);
+    }
+
+    else{
+        if(head->left==NULL&&head->right==NULL)
+        {
+            delete head;
+            head = NULL;
+        }
+        if(head->left==NULL&&head->right!=NULL)
+        {
+            Node*temp = head->right;
+            delete head;
+            return temp;
+        }
+        if (head->left!= NULL && head->right == NULL) {
+            Node* temp = head->left;
+            delete head;
+            return temp;
+        }
+
+        else{
+            Node*temp = findmin(head->right);
+            head->data = temp->data;
+            head->right = deletenode(head->right,temp->data);
+
+        }
+    }
+
+    return head;
 }
 
 int main()
@@ -85,8 +176,18 @@ int main()
         head = insert(head,data);
 
     }
-    printPreorder(head);
+    //printPreorder(head);
+    //Inorder(head);
+    //postorder(head);
+
+    //cout<<search(head,8);
+    //findmaximum(head);
+    //findminimum(head);
+
+    deletenode(head,10);
+
     Inorder(head);
-    postorder(head);
+
+
 
 }

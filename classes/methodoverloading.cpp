5d18f338d133781d3b4c1a54d61457c724b3dabc
RAHUL YADAV <imdotrahul@gmail.com>
2024-09-01 15:33:45 +0530

    learned method overloading

---
+++ classes/methodoverloading.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+class Animal{
+    public:
+        void legs(){
+            cout<<"Many Animals legs shown below:"<<"\n";
+
+   }
+    void legs(int n){
+            cout<<"dogs have "<<n<<" legs"<<"\n";
+        }
+        void legs(int n, int hands){
+            cout<<"dogs have "<<n<<" legs and "<<hands<<" hands"<<"\n";
+        }
+};
+
+
+int main()
+{
+    Animal h;
+    h.legs();
+    h.legs(4);
+    h.legs(2,2);
+
+}
